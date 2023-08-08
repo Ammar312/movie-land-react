fc58f446a5c1b1ea288ae4b80bc399b9e9521152
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-08-09 01:08:25 +0500

    api secure

--- movie-land-folder/src/App.jsx
+++ movie-land-folder/src/App.jsx
@@ -4,7 +4,7 @@ import SearchBar from "./components/SearchBar";
 import MoviesCard from "./components/MoviesCard";
 import "./App.css";
 // 3f92b819
-const API = `http://www.omdbapi.com/?i=tt3896198&apikey=3f92b819`;
+const API = `https://www.omdbapi.com/?i=tt3896198&apikey=3f92b819`;
 const App = () => {
   const [movies, setMovies] = useState([]);
   const searchMovies = async (title) => {
